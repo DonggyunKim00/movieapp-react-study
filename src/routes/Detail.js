2ad7a2cb53a44b98ba250b7defb41898c51b442c
DOnggyunKim00 <donggyunkim12@gmail.com>
2022-07-31 15:13:37 +0900

    version5

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 function Detail() {
-  const { id } = useParams();
-  const getMovies = async () => {
+  const [movieInfo, setMovieInfo] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const { id } = useParams(); // id 값을 찾는곳
+  const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    console.log(json);
+    ).json(); // 내가 클릭한 영화의 정보가 들어있음
+    setMovieInfo(json.data.movie.url);
+    setLoading((current) => !current);
   };
   useEffect(() => {
-    getMovies();
+    getMovie();
   }, []);
 
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <h1>
+          <a href={movieInfo} target='_blank'>
+            Go to torrent page
+          </a>
+        </h1>
+      )}
+    </div>
+  );
 }
-
 export default Detail;
